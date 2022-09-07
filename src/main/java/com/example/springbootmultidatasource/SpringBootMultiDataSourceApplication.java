048b6c57389cf9c6642e2d1d9215c0d17630df37
suhaas <suhaasv@gmail.com>
2022-09-07 00:18:25 -0400

    second commit

---
+++ src/main/java/com/example/springbootmultidatasource/SpringBootMultiDataSourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmultidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiDataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiDataSourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmultidatasource/SpringBootMultiDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmultidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiDataSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
